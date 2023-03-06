3fc56eb2ef6258adaf72b7c5bea1a1a429d8e768
parth_agrawal <parth.agrawal@bizzappdev.com>
2023-03-06 11:24:25 +0530

    [add][T00309]school-Add changes

--- School_Management/models/mrp_production.py
+++
@@ -1,8 +0,0 @@
-from odoo import fields, models
-
-
-class ManufacturingInherit(models.Model):
-    _inherit = "mrp.production"
-
-    manufacturing_description = fields.Char(string="Manufacturing description")
-
