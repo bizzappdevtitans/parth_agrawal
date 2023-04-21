21a5d452eb9b4e996d82333b2ba3c512e8396526
parth_agrawal <parth.agrawal@bizzappdev.com>
2023-04-21 12:32:17 +0530

    [WIP][T6235]create sale order through payload-BAD-PAA

---
+++ sale_order_mapping/README.rst
@@ -0,0 +1,54 @@
+=======================
+**Sale Order Mapping**
+=======================
+
+**Description**
+***************
+
+* We can use this module to create new sale order by clicking create button in
+wizard.
+
+**Author**
+**********
+
+* #N/A
+
+**Configuration**
+*****************
+
+* We need to just click on sub menu on sale module to open wizard and create new sale
+order.
+
+**Used by**
+***********
+
+* #N/A
+
+**Installation**
+****************
+
+* #N/A
+
+**Usage**
+*********
+
+* By changing the payload of the dictionary we can create new user for the res_partner
+as well as compare the product's default code with the reference given in payload.
+
+* If default code does not matches the validation error will be generated.
+
+* We just need to click in sale app's order menu and then select the create sale
+order button to use the wizard functionality.
+
+* We can create new customer if the payload's customer value not found in contact
+module.
+
+**Known issues/Roadmap**
+************************
+
+* #N/A
+
+**Changelog**
+*************
+
+* #N/A
---
+++ sale_order_mapping/wizard/sale_order_create_wizard.py
@@ -0,0 +1,72 @@
+from odoo import models
+from odoo.exceptions import ValidationError
+
+
+class CreateSaleOrderWizard(models.TransientModel):
+    _name = "sale.order.create.wizard"
+    _description = "create sale order through wizard"
+
+    def get_sale_order_payload(self):
+        """This method contains the sale order's payload"""
+        sale_order_payload = {
+            "name": "PL/AT:123456",
+            "customer": {
+                "name": "XYZ",
+                "address": {"city": "Pune", "zip": "10ji033", "phone": "990022933"},
+            },
+            "sale_lines": [
+                {"productId": "5112976146", "quantity": 3},
+                {"productId": "5112974885", "quantity": 15},
+                {"productId": "5112973994", "quantity": 3},
+            ],
+        }
+        return sale_order_payload
+
+    def action_on_click_create(self):
+        """This method use sale order's payload to create new sale order"""
+        sale_order_payload = self.get_sale_order_payload()
+        customer = self.env["res.partner"].search(
+            [("name", "=", sale_order_payload["customer"]["name"])]
+        )
+
+        if not customer:
+            customer = self.env["res.partner"].create(
+                {
+                    "name": sale_order_payload["customer"]["name"],
+                    "city": sale_order_payload["customer"]["address"]["city"],
+                    "zip": sale_order_payload["customer"]["address"]["zip"],
+                    "phone": sale_order_payload["customer"]["address"]["phone"],
+                }
+            )
+
+        order_lines = []
+
+        for line in sale_order_payload["sale_lines"]:
+            product = self.env["product.product"].search(
+                [("default_code", "=", line["productId"])]
+            )
+            if not product:
+                raise ValidationError(
+                    f"Product with default_code '{line['productId']}' not found"
+                )
+            order_lines.append(
+                (0, 0, {"product_id": product.id, "product_uom_qty": line["quantity"]})
+            )
+
+        sale_order = self.env["sale.order"].create(
+            {
+                "name": sale_order_payload["name"],
+                "partner_id": customer.id,
+                "order_line": order_lines,
+            }
+        )
+
+        data = {
+            "name": "Sale Order",
+            "view_mode": "tree,form",
+            "res_model": "sale.order",
+            "res_id": sale_order.id,
+            "type": "ir.actions.act_window",
+        }
+
+        return data
